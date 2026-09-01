dd7a28527e889e05c827f40cdff1fc3256410e7f
Tushar kumar <tk253206.in>
2026-09-01 19:04:16 +0530

    remove deleted files

--- 01-variables.js
+++
@@ -1,8 +0,0 @@
-const accountID = 11223344
-let accountEmail = "tushar@email.com"
-var accountpassword = "12345678"
-let accountcity = "uttarpardesh"
-
- console.log(accountID);
-
-console.table([accountID,accountEmail,accountpassword,accountcity]);
