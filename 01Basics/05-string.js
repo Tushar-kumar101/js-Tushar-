9526bd2d656c8e151ffe2dc499e3693bc2e8d653
Tushar kumar <tk253206.in>
2026-09-03 18:12:45 +0530

    string

---
+++ 01Basics/05-string.js
@@ -0,0 +1,18 @@
+//const name = "Tushar";
+//const repocount = 69;
+
+// console.log(name + repocount + "value");
+
+//console.log(`hello my name is ${name} and my repocount is ${repocount}`);
+
+ //const gamename = "Tushar kumar";
+
+ //console.log(gamename[0]);
+  //console.log(gamename[5]);
+ //console.log(gamename[2]);
+
+ console.log(gamename.charAt(2));
+ console.log(gamename.indexOf('m'));
+
+ const newString = gamename.substring(0 , 4);
+ console.log(newString);
