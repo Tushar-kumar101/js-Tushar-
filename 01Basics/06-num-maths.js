a1e6fcfa5a0bb65ed3058ce7d46723eb3d041e39
Tushar kumar <tk253206.in>
2026-09-08 00:06:27 +0530

    number

---
+++ 01Basics/06-num-maths.js
@@ -0,0 +1,29 @@
+//const score = 400 
+//console.log(score);
+
+//const balance = new Number(1969)
+//console.log(balance);
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(1));
+
+//const othernumber = 123.898
+//console.log(othernumber.toPrecision(3));
+
+//const hundreds = 10000000
+//console.log(hundreds.toLocaleString('en-IN'));
+
+//++++++++++++++++++++++++++++++++ MATHS +++++++++++++++++++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.3));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.2));
+console.log(Math.min(4,5,6,8));
+console.log(Math.max(4,5,6,8));
+
+
+console.log(Math.random());
+
+console.log(Math.random()*10);
