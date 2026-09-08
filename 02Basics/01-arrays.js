315418e7c2c540e801e72293c4081d6ed90e0500
Tushar kumar <tk253206.in>
2026-09-08 15:17:25 +0530

    arrays

---
+++ 02Basics/01-arrays.js
@@ -0,0 +1,9 @@
+//arrays
+
+const myArray = [1,2,3,4,6,7];
+const myHeros = ["shaktiman","nagraj"];
+const myArray2 = new Array(1,2,3,4,5,6,7);
+
+console.log(myArray[0]);
+console.log(myHeros[0]);
+console.log(myArray2[0]);
