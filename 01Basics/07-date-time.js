08f33831e98dc1d4e80f793ead95ef8b4e6027f4
Tushar kumar <tk253206.in>
2026-09-08 14:52:30 +0530

    date and time

--- 01Basics/07-date-time.js
+++ 01Basics/07-date-time.js
@@ -1,4 +1,15 @@
 //Dates 
 
-let myDate = new Date();
-console.log("myDate");
+//let myDate = new Date();
+//console.log(myDate.toString());
+//console.log(myDate.toDateString());
+//console.log(myDate.toLocaleDateString());
+//console.log(myDate.toJSON());
+//console.log(myDate.toLocaleTimeString());
+//console.log(typeof myDate);
+
+//let myCreatedDate = new Date(2026, 0, 25, 4 ,52);
+//console.log(myCreatedDate .toLocaleString());
+
+let myTimestamp = Date.now();
+console.log(myTimestamp);  
