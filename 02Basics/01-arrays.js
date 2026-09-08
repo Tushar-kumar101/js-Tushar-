2fa752d3f29e65b7b314327712786edbf6341f79
Tushar kumar <tk253206.in>
2026-09-08 16:07:49 +0530

    arrays pt 1

--- 02Basics/01-arrays.js
+++ 02Basics/01-arrays.js
@@ -4,6 +4,24 @@ const myArray = [1,2,3,4,6,7];
 const myHeros = ["shaktiman","nagraj"];
 const myArray2 = new Array(1,2,3,4,5,6,7);
 
-console.log(myArray[0]);
-console.log(myHeros[0]);
-console.log(myArray2[0]);
+console.log(myArray[5]);
+console.log(myHeros[0,1]);
+console.log(myArray2[5]);
+
+//Array method 
+
+//myArray.push(8,9,10);
+//console.log(myArray);
+
+//myArray.pop();
+//console.log(myArray);
+
+//console.log(myArray.includes(9));
+//console.log(myArray.indexOf(9));
+
+//const newArray = myArray.join();
+//console.log(typeof newArray);
+//console.log(myArray);
+
+// slice or splice
+ 
