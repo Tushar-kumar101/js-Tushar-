943101837f129b8f60ed0e0bd528fa8b9d462ec9
Tushar kumar <tk253206.in>
2026-09-10 12:53:13 +0530

    objects

---
+++ 02Basics/04-objects.js
@@ -0,0 +1,8 @@
+//const facebookuser = new Object()
+
+const facebookuser = {}
+
+facebookuser.ID = "001"
+facebookuser.name = "Tushar Kumar"
+facebookuser.isLoggedIn = false 
+console.log(facebookuser);
